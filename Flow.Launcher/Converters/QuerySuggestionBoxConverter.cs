bc6e314c35b65733bed3c48fefe451d784784745
No Name <>
2024-02-24 09:10:32 +0600

    Code cleanup: Flow.Launcher/Converters

--- Flow.Launcher/Converters/QuerySuggestionBoxConverter.cs
+++ Flow.Launcher/Converters/QuerySuggestionBoxConverter.cs
@@ -12,31 +12,12 @@ namespace Flow.Launcher.Converters
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            if (values.Length != 3)
-            {
+            if (values is not [TextBox queryTextBox, ResultViewModel selectedItem, string queryText] ||
+                string.IsNullOrEmpty(queryText))
                 return string.Empty;
-            }
-            var QueryTextBox = values[0] as TextBox;
-
-            var queryText = (string)values[2];
-
-            if (string.IsNullOrEmpty(queryText))
-                return string.Empty;
-
-            // second prop is the current selected item result
-            var val = values[1];
-            if (val == null)
-            {
-                return string.Empty;
-            }
-            if (!(val is ResultViewModel))
-            {
-                return System.Windows.Data.Binding.DoNothing;
-            }
 
             try
             {
-                var selectedItem = (ResultViewModel)val;
 
                 var selectedResult = selectedItem.Result;
                 var selectedResultActionKeyword = string.IsNullOrEmpty(selectedResult.ActionKeywordAssigned) ? "" : selectedResult.ActionKeywordAssigned + " ";
@@ -48,19 +29,17 @@ namespace Flow.Launcher.Converters
 
                 // For AutocompleteQueryCommand.
                 // When user typed lower case and result title is uppercase, we still want to display suggestion
-                selectedItem.QuerySuggestionText = queryText + selectedResultPossibleSuggestion.Substring(queryText.Length);
+                selectedItem.QuerySuggestionText = string.Concat(queryText, selectedResultPossibleSuggestion.AsSpan(queryText.Length));
 
-                // Check if Text will be larger then our QueryTextBox
-                System.Windows.Media.Typeface typeface = new Typeface(QueryTextBox.FontFamily, QueryTextBox.FontStyle, QueryTextBox.FontWeight, QueryTextBox.FontStretch);
+                // Check if Text will be larger than our QueryTextBox
+                Typeface typeface = new Typeface(queryTextBox.FontFamily, queryTextBox.FontStyle, queryTextBox.FontWeight, queryTextBox.FontStretch);
                 // TODO: Obsolete warning?
-                System.Windows.Media.FormattedText ft = new FormattedText(QueryTextBox.Text, System.Globalization.CultureInfo.DefaultThreadCurrentCulture, System.Windows.FlowDirection.LeftToRight, typeface, QueryTextBox.FontSize, Brushes.Black);
+                var ft = new FormattedText(queryTextBox.Text, CultureInfo.DefaultThreadCurrentCulture, System.Windows.FlowDirection.LeftToRight, typeface, queryTextBox.FontSize, Brushes.Black);
 
-                var offset = QueryTextBox.Padding.Right;
+                var offset = queryTextBox.Padding.Right;
 
-                if ((ft.Width + offset) > QueryTextBox.ActualWidth || QueryTextBox.HorizontalOffset != 0)
-                {
+                if (ft.Width + offset > queryTextBox.ActualWidth || queryTextBox.HorizontalOffset != 0)
                     return string.Empty;
-                };
 
                 return selectedItem.QuerySuggestionText;
             }
